74eaa840a432f6b0dd5d503338676445d71eac4d
Alex <alexwhite123@gmail.com>
2014-10-09 22:16:06 -0400

    Two pictures loading.

--- src/application/GUIController.java
+++ src/application/GUIController.java
@@ -7,12 +7,13 @@ import javafx.scene.image.ImageView;
 
 public class GUIController {
 	
-	@FXML ScrollPane pictureframe;
+	@FXML ScrollPane pictureframe1;
+	@FXML ScrollPane pictureframe2;
 	
 	int count = 0;
 	String imagename = null;
 	
-	@FXML public void displayImage(){
+	@FXML public void displayImage1(){
 		
 		if(count%3 == 0) imagename = "colors.jpg";
 		if(count%3 == 1) imagename = "letters.jpg";
@@ -26,9 +27,29 @@ public class GUIController {
 		ImageView picture = new ImageView();
 		picture.setImage(image);
 		
-		pictureframe.setContent(picture);
+		pictureframe1.setContent(picture);
          
         System.out.println("What the fuck.");
 	}
+	
+	@FXML public void displayImage2(){
+		
+		if(count%3 == 0) imagename = "colors.jpg";
+		if(count%3 == 1) imagename = "letters.jpg";
+		if(count%3 == 2) imagename = "sample.jpg";
+		count++;
+		
+		/* LOAD THE IMAGE */
+		Image image = new Image(imagename);
+		
+		/* SET IT AS AN IMAGE VIEW */
+		ImageView picture = new ImageView();
+		picture.setImage(image);
+		
+		pictureframe2.setContent(picture);
+         
+        System.out.println("What the fuck.");
+	}
+	
 	
 }
